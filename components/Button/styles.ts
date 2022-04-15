162d619c8ddb268b7b49a677a02bb5f2fb902947
JustZoB <justzofb@gmail.com>
2022-04-15 15:18:29 +0600

    Change styles of button

--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -4,12 +4,13 @@ import colors from "../../utils/colors";
 
 export const StyledButton = styled.button<ButtonProps>`
   width: ${(props) => (props.size === 'big' ? '100%' : 'fit-content')};
+  height: fit-content;
   background-color: ${(props) => (props.theme != 'primary' ? colors.white : colors.lightRed)};
   color: ${(props) => (props.theme != 'primary' ? colors.lightRed : colors.white)};
   font-size: 18px;
   line-height: 20px;
   font-weight: 700;
-  padding: 26px 38px;
+  padding: 20px 38px;
   border: 0;
   border-radius: 4px;
   cursor: pointer;
