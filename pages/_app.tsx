3efd284e13f3e905e2a253e8d330c02b625e5fb8
JustZoB <justzofb@gmail.com>
2022-04-19 12:25:28 +0600

    Remove broke-logIn code for a while

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,7 @@ import { persistor, store } from "../store/store"
 import { GlobalStyle } from "../styles/globals"
 
 function MyApp({ Component, pageProps }) {
-  AxiosInterceptors.setup(store);
-  
+  // AxiosInterceptors.setup(store);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
